aab3fa175d7b3b25496aad0b972287515b72c81b
Jonathan Mendes Carvalheiro <jonathanmendescarvalheiro@Jonathans-MacBook-Pro.local>
2019-08-07 17:49:39 +0100

    fix: Added complexity

--- CyclomaticComplexity.java
+++ CyclomaticComplexity.java
@@ -1,67 +1,65 @@
+
 public class CyclomaticComplexity {
-    
-    public void complex(){
-        for (int i =0;i<1000; i++){
-            if(i<100){
-                for(int x=0; x<1000; x++){
-                    if(x<100){
-                        if(x==50){
-                            x=51
-                        }
-                        else{
-                            int y = 12312313;
-                            gettingComplex()
-                            for (int i =0;i<1000; i++){
-            if(i<100){
-                for(int z=0; z<1000; z++){
-                    if(z<100){
-                        if(z==50){
-                            z=51
-                        }
-                        else{
-                            int w = 12312313;
-                        }
 
-                    }
-                }
-                else {
-                    int l= 10100;
-                }
-            }
-        }
-                        }
+ public void complex() {
+  for (int i = 0; i < 1000; i++) {
+   if (i < 100) {
+    for (int x = 0; x < 1000; x++) {
+     if (x < 100) {
+      if (x == 50) {
+       x = 51;
+      } else {
+       int y = 12312313;
+       gettingComplex();
+       for (int f = 0; f < 1000; f++) {
+        if (f < 100) {
+         for (int z = 0; z < 1000; z++) {
+          if (z < 100) {
+           if (z == 50) {
+            z = 51;
+           } else {
+            int w = 12312313;
+           }
 
-                    }
-                }
-                else {
-                    int j= 10100;
-                    gettingComplex()
-                }
-            }
+          }
+         else {
+          int l = 10100;
+         }
         }
+        }
+       }
+      }
+
+     }
+     else {
+     int j = 10100;
+     gettingComplex();
     }
-
-    public void gettingComplex(){
-        for (int i =0;i<1000; i++){
-            if(i<100){
-                for(int x=0; x<1000; x++){
-                    if(x<100){
-                        if(x==50){
-                            x=51
-                        }
-                        else{
-                            int y = 12312313;
-                        }
-
-                    }
-                }
-                else {
-                    int j= 10100;
-                }
-            }
-        }
     }
+   }
+  }
+ }
 
-    //Second commit
-    //Thirth commit 
+ public void gettingComplex() {
+  for (int i = 0; i < 1000; i++) {
+   if (i < 100) {
+    for (int x = 0; x < 1000; x++) {
+     if (x < 100) {
+      if (x == 50) {
+       x = 51;
+      } else {
+       int y = 12312313;
+      }
+
+     }
+    else {
+     int j = 10100;
+    }
+   }
+  }
+ }
+ }
+
+ //Second commit
+ //Thirth commit 
 }
