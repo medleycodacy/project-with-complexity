275d9af79200ba69a1e8e71546c8bb9b8a567770
Jonathan Mendes Carvalheiro <jonathanmendescarvalheiro@Jonathans-MacBook-Pro.local>
2019-08-07 16:47:32 +0100

    feat: Added complexity

--- CyclomaticComplexity.java
+++ CyclomaticComplexity.java
@@ -10,6 +10,47 @@ public class CyclomaticComplexity {
                         }
                         else{
                             int y = 12312313;
+                            gettingComplex()
+                            for (int i =0;i<1000; i++){
+            if(i<100){
+                for(int z=0; z<1000; z++){
+                    if(z<100){
+                        if(z==50){
+                            z=51
+                        }
+                        else{
+                            int w = 12312313;
+                        }
+
+                    }
+                }
+                else {
+                    int l= 10100;
+                }
+            }
+        }
+                        }
+
+                    }
+                }
+                else {
+                    int j= 10100;
+                    gettingComplex()
+                }
+            }
+        }
+    }
+
+    public void gettingComplex(){
+        for (int i =0;i<1000; i++){
+            if(i<100){
+                for(int x=0; x<1000; x++){
+                    if(x<100){
+                        if(x==50){
+                            x=51
+                        }
+                        else{
+                            int y = 12312313;
                         }
 
                     }
