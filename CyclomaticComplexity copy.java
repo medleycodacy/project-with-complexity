fab1b5aba222d5392df3a446f37a32fcf6f48d83
Jonathan Mendes Carvalheiro <jonathanmendescarvalheiro@Jonathans-MacBook-Pro.local>
2019-08-07 16:28:11 +0100

    feat: Add copy

---
+++ CyclomaticComplexity copy.java
@@ -0,0 +1,26 @@
+public class CyclomaticComplexity {
+    
+    public void complex(){
+        for (int i =0;i<1000; i++){
+            if(i<100){
+                for(int x=0; x<1000; x++){
+                    if(x<100){
+                        if(x==50){
+                            x=51
+                        }
+                        else{
+                            int y = 12312313;
+                        }
+
+                    }
+                }
+                else {
+                    int j= 10100;
+                }
+            }
+        }
+    }
+
+    //Second commit
+    //Thirth commit 
+}
